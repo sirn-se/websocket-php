b30ae630f63fcf2b1610da335b5351f5ab486e72
Sören Jensen <sirn@sirn.se>
2026-01-14 19:06:41 +0100

    Copyright 2026 (#149)

--- src/Message/Text.php
+++ src/Message/Text.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Copyright (C) 2014-2025 Textalk and contributors.
+ * Copyright (C) 2014-2026 Textalk and contributors.
  * This file is part of Websocket PHP and is free software under the ISC License.
  */
 
